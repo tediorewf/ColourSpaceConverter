4971bc05a0851ee0ac6cf09f4b3f8787339f43f4
grishagr9 <grisha-rnd10@yandex.ru>
2022-09-18 14:43:45 +0300

    'start'

--- ColourSpaceConverter/RGBToGrayScaleForm.cs
+++ ColourSpaceConverter/RGBToGrayScaleForm.cs
@@ -16,5 +16,38 @@ namespace ColourSpaceConverter
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                TrySetImage(ofd.FileName);
+            }
+        }
+        private void TrySetImage(string filename)
+        {
+            try
+            {
+               /* var baseBitmap = new Bitmap(filename);
+                var colourChannels = ExctractChannels(baseBitmap);
+                var histogramBitmap = CreateHistogram(baseBitmap, histogramPictureBox.Size);
+                baseImagePictureBox.Image = baseBitmap;
+                redChannelPictureBox.Image = colourChannels[0];
+                blueChannelPictureBox.Image = colourChannels[1];
+                greenChannelPictureBox.Image = colourChannels[2];
+                histogramPictureBox.Image = histogramBitmap;*/
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
