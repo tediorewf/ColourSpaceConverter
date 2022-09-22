5d2ae32d4cd35ec207f3ab85f3d58b0df8b80b61
Restyled.io <commits@restyled.io>
2022-09-22 16:45:18 +0000

    Restyled by astyle

--- ColourSpaceConverter/PictureForm.cs
+++ ColourSpaceConverter/PictureForm.cs
@@ -10,22 +10,22 @@ using System.Windows.Forms;
 
 namespace ColourSpaceConverter
 {
-    public partial class PictureForm : Form
+public partial class PictureForm : Form
+{
+    public PictureForm(Bitmap bitmap)
     {
-        public PictureForm(Bitmap bitmap)
-        {
-            InitializeComponent();
-            pictureBox1.Image = bitmap;
-            int w = bitmap.Width;
-            int h = bitmap.Height;
-            this.Size = new Size(w + pictureBox1.Location.X * 3, h + 89 + pictureBox1.Location.Y);
-            pictureBox1.Size = new Size(w + pictureBox1.Location.X * 3, h + 89 + pictureBox1.Location.Y);
+        InitializeComponent();
+        pictureBox1.Image = bitmap;
+        int w = bitmap.Width;
+        int h = bitmap.Height;
+        this.Size = new Size(w + pictureBox1.Location.X * 3, h + 89 + pictureBox1.Location.Y);
+        pictureBox1.Size = new Size(w + pictureBox1.Location.X * 3, h + 89 + pictureBox1.Location.Y);
 
-        }
-
-        private void PictureForm_Load(object sender, EventArgs e)
-        {
-
-        }
     }
+
+    private void PictureForm_Load(object sender, EventArgs e)
+    {
+
+    }
+}
 }
--- ColourSpaceConverter/RGBToGrayScaleForm.Designer.cs
+++ ColourSpaceConverter/RGBToGrayScaleForm.Designer.cs
@@ -1,128 +1,128 @@
 ﻿namespace ColourSpaceConverter
 {
-    partial class RGBToGrayScaleForm
+partial class RGBToGrayScaleForm
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        if (disposing && (components != null))
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
+            components.Dispose();
         }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(122, 51);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Выбрать изображение";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(140, 12);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 51);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "В оттенки серого\r\nвариант 1";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(290, 12);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(141, 51);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "В оттенки серого \r\nвариант 2";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 89);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(532, 251);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(437, 12);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(107, 51);
-            this.button4.TabIndex = 4;
-            this.button4.Text = " Разность";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.button4_Click);
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(550, 12);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(125, 50);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Гистограмма";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
-            // RGBToGrayScaleForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(683, 363);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.Name = "RGBToGrayScaleForm";
-            this.Text = "Преобразование изображения из RGB в оттенки серого";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
-        }
-
-        #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        base.Dispose(disposing);
     }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        this.button1 = new System.Windows.Forms.Button();
+        this.button2 = new System.Windows.Forms.Button();
+        this.button3 = new System.Windows.Forms.Button();
+        this.pictureBox1 = new System.Windows.Forms.PictureBox();
+        this.button4 = new System.Windows.Forms.Button();
+        this.button5 = new System.Windows.Forms.Button();
+        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+        this.SuspendLayout();
+        //
+        // button1
+        //
+        this.button1.Location = new System.Drawing.Point(12, 12);
+        this.button1.Name = "button1";
+        this.button1.Size = new System.Drawing.Size(122, 51);
+        this.button1.TabIndex = 0;
+        this.button1.Text = "Выбрать изображение";
+        this.button1.UseVisualStyleBackColor = true;
+        this.button1.Click += new System.EventHandler(this.button1_Click);
+        //
+        // button2
+        //
+        this.button2.Location = new System.Drawing.Point(140, 12);
+        this.button2.Name = "button2";
+        this.button2.Size = new System.Drawing.Size(144, 51);
+        this.button2.TabIndex = 1;
+        this.button2.Text = "В оттенки серого\r\nвариант 1";
+        this.button2.UseVisualStyleBackColor = true;
+        this.button2.Click += new System.EventHandler(this.button2_Click);
+        //
+        // button3
+        //
+        this.button3.Location = new System.Drawing.Point(290, 12);
+        this.button3.Name = "button3";
+        this.button3.Size = new System.Drawing.Size(141, 51);
+        this.button3.TabIndex = 2;
+        this.button3.Text = "В оттенки серого \r\nвариант 2";
+        this.button3.UseVisualStyleBackColor = true;
+        this.button3.Click += new System.EventHandler(this.button3_Click);
+        //
+        // pictureBox1
+        //
+        this.pictureBox1.Location = new System.Drawing.Point(12, 89);
+        this.pictureBox1.Name = "pictureBox1";
+        this.pictureBox1.Size = new System.Drawing.Size(532, 251);
+        this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+        this.pictureBox1.TabIndex = 3;
+        this.pictureBox1.TabStop = false;
+        //
+        // button4
+        //
+        this.button4.Location = new System.Drawing.Point(437, 12);
+        this.button4.Name = "button4";
+        this.button4.Size = new System.Drawing.Size(107, 51);
+        this.button4.TabIndex = 4;
+        this.button4.Text = " Разность";
+        this.button4.UseVisualStyleBackColor = true;
+        this.button4.Click += new System.EventHandler(this.button4_Click);
+        //
+        // button5
+        //
+        this.button5.Location = new System.Drawing.Point(550, 12);
+        this.button5.Name = "button5";
+        this.button5.Size = new System.Drawing.Size(125, 50);
+        this.button5.TabIndex = 5;
+        this.button5.Text = "Гистограмма";
+        this.button5.UseVisualStyleBackColor = true;
+        this.button5.Click += new System.EventHandler(this.button5_Click);
+        //
+        // RGBToGrayScaleForm
+        //
+        this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(683, 363);
+        this.Controls.Add(this.button5);
+        this.Controls.Add(this.button4);
+        this.Controls.Add(this.pictureBox1);
+        this.Controls.Add(this.button3);
+        this.Controls.Add(this.button2);
+        this.Controls.Add(this.button1);
+        this.Margin = new System.Windows.Forms.Padding(2);
+        this.Name = "RGBToGrayScaleForm";
+        this.Text = "Преобразование изображения из RGB в оттенки серого";
+        ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+        this.ResumeLayout(false);
+        this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.Button button3;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button button4;
+    private System.Windows.Forms.Button button5;
+}
 }
