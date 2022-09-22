8548318775c857470f7c4c8c22864cf9194e99fa
Restyled.io <commits@restyled.io>
2022-09-22 16:45:19 +0000

    Restyled by clang-format

--- ColourSpaceConverter/PictureForm.cs
+++ ColourSpaceConverter/PictureForm.cs
@@ -8,24 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ColourSpaceConverter
-{
-public partial class PictureForm : Form
-{
-    public PictureForm(Bitmap bitmap)
-    {
-        InitializeComponent();
-        pictureBox1.Image = bitmap;
-        int w = bitmap.Width;
-        int h = bitmap.Height;
-        this.Size = new Size(w + pictureBox1.Location.X * 3, h + 89 + pictureBox1.Location.Y);
-        pictureBox1.Size = new Size(w + pictureBox1.Location.X * 3, h + 89 + pictureBox1.Location.Y);
+namespace ColourSpaceConverter {
+public partial class PictureForm : Form {
+  public PictureForm(Bitmap bitmap) {
+    InitializeComponent();
+    pictureBox1.Image = bitmap;
+    int w = bitmap.Width;
+    int h = bitmap.Height;
+    this.Size = new Size(w + pictureBox1.Location.X * 3,
+                         h + 89 + pictureBox1.Location.Y);
+    pictureBox1.Size = new Size(w + pictureBox1.Location.X * 3,
+                                h + 89 + pictureBox1.Location.Y);
+  }
 
-    }
-
-    private void PictureForm_Load(object sender, EventArgs e)
-    {
-
-    }
+  private void PictureForm_Load(object sender, EventArgs e) {}
 }
 }
